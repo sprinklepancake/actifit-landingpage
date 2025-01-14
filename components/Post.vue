8599c5da60341ac605725c3739805bedfd1ec141
sprinklepancake <hassan.najjar11lb@gmail.com>
2025-01-14 20:16:12 +0200

    anchor tag fix

--- components/Post.vue
+++ components/Post.vue
@@ -22,7 +22,7 @@
 			<i class="fas fa-reply text-brand"></i>&nbsp;
       <UserHoverCard :username="post.parent_author" />
         </div>
-    </a>
+    </span>
 </div>
   </div>        
   <div class="row">
